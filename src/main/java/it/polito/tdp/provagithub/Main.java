e1c5d6a1740244feb1b5dd75bbf8d5879f892644
Peppe <Peppe@172.22.104.64>
2022-03-07 09:19:11 +0100

    modifica1

--- src/main/java/it/polito/tdp/provagithub/Main.java
+++ src/main/java/it/polito/tdp/provagithub/Main.java
@@ -5,3 +5,5 @@ public class Main {
         EntryPoint.main(args);
     }
 }
+//tasto dx su cartella, Team, share project, crea una repository locale nella cartella del workspace (spunta la voce e poi crea la Rep)
+// tasto dx su cartella, Team, commit, spostare i file interessati di unstaged changes a staged changes
